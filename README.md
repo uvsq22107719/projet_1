bf7a22ff1a504d7d449b7a8f1f3f4849033a337a
Guillaume Emerdjian <guillaume.emerdjian@ens.uvsq.fr>
2022-02-11 09:15:07 +0100

    ajout tas_de_sable.py

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 IN200: projet sur la simulation de l’écoulement d’un tas de sable
+
+groupe BI 2
+Guillaume EMERDJIAN
+Victor DE BAETS
+Léa OSTER
+Maxime BOUAMRA
+https://github.com/uvsq22107719/projet_1
---
+++ tas_de_sable.py
@@ -0,0 +1,8 @@
+#########################################
+# groupe BI 2
+# Guillaume EMERDJIAN
+# Victor DE BAETS
+# Léa OSTER
+# Maxime BOUAMRA
+# https://github.com/uvsq22107719/projet_1
+#########################################
