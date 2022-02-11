3862cc6d3e1cc4db79c02b2a467e1be61560aea5
Léa <lea.oster@ens.uvsq.fr>
2022-02-11 09:21:36 +0100

    a

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -2,7 +2,7 @@
 # groupe BI 2
 # Guillaume EMERDJIAN
 # Victor DE BAETS
-# Léa OSTER
+# Lhéa OSTER
 # Maxime BOUAMRA
 # https://github.com/uvsq22107719/projet_1
 #########################################
